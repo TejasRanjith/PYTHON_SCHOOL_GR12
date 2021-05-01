b81ba132301ea3bd6209a7a5409872eb735dfbc1
Tejas <tejasranjith035611@gmail.com>
2021-05-01 19:04:29 +0400

    Commit_0011

--- Computer_Science/RecordFile/Section-C/RFC1.py
+++ Computer_Science/RecordFile/Section-C/RFC1.py
@@ -13,12 +13,9 @@ def display():
         for i in range(1, 8):
             print(f"{i}. "+f.readline())
 
-def create_v():
-    with open("File.txt", "w") as f:f.write('''abcdefghijklmnopqrstuvwxyz\nHi my name is Tejas\nI study in class 12-C''')
-
 def v_count():
     c, v = 0, 0
-    with open("File.txt", "r") as f:
+    with open("Emirates.txt", "r") as f:
         para = f.read().lower()
     for elem in para:
             if elem in ["a", "e", "i", "o", "u"]:
@@ -32,21 +29,38 @@ def v_count():
     return c, v
 
 def w_count():
-    with open("File.txt", "r") as f:
-        print(len(f.read().replace("\n", " ").split(sep=" ")))
+    with open("Emirates.txt", "r") as f:
+        return (len(f.read().replace("\n", " ").split(sep=" "))-1)
+
 
 #! Main
 while True:
-    print("<<----  MENU  ---->>");print()
-    print("1.) To Create a Text file 'Emirates' and enter the names of 7 Emirates --> ce")
+    print("\n<<----  MENU  ---->>\n")
     print("2.) To read the file 'Emirates', line by line --> re")
     print("3.) To count the number of vowels and consonants in present in the text file --> cv")
-    print("4.) To count the number of the words in the text file --> cw");print();opt = input('Option --> ');opt = opt.lower()
-    if opt in ["ce", "re", "cv", "cw"]:
-        if opt == "ce":print();create();print("The file has been created......")
-        elif opt == "re":display()
-        elif opt == "cv":a,b = v_count();print(a,b)
-        elif opt == "cw":(w_count())
-        else:print("Invalid Option......")
-    elif opt == "0":timer(3);print("Program exited with exit code 0");break
-    else:print("Invalid Option......")
+    print("4.) To count the number of the words in the text file --> cw")
+    print("5.) To stop the main program --> 0\n")
+    opt = input('Option --> ')
+    opt = opt.lower()
+    if opt in ["re", "cv", "cw"]:
+        print()
+        if opt == "re":
+            display()
+        elif opt == "cv":
+            a,b = v_count()
+            print("Number of Vowels: ",b)
+            print("Number of Consonants: ",a)
+        elif opt == "cw":
+            print("The Number of words in the file: ",w_count(),sep = "\n")
+        else:
+            print("Invalid Option......")
+    elif opt == "0":
+        timer(1)
+        print("\n"+"Thank you for using the string modifier program."+"\n")
+        timer(1)
+        print("Hope to see you soon in string modifier program version 1.2.0"+"\n"+"\n")
+        timer(1.5)
+        print("Program exited with exit code 0"+"\n")
+        break
+    else:
+        print("Invalid Option......")
---
+++ Computer_Science/RecordFile/Section-C/RFC2.py
@@ -0,0 +1,72 @@
+def timer(n):
+    import time
+    time.sleep(n)
+
+def dots():
+    with open("poem.txt","r") as f:
+        return (f.read().count("."))
+
+def count_I():
+    with open("poem.txt","r") as f:
+        return f.read().count(" "+"I"+" ")
+
+def lines_tT():
+    result = []
+    with open("poem.txt","r") as f:
+        for sent in f.read().split(sep = "\n"):
+            if sent[0] == "T" or sent[0] == "t":
+                result.append(sent)
+        return result
+
+def lines():
+    with open("poem.txt","r") as f:
+        l =  f.read().split(sep = "\n")
+        return l[0],l[-1]
+
+def longest():
+    with open("poem.txt","r") as f:
+        l_sent = f.read().split(sep = "\n")
+        l_len = []
+        for sent in l_sent:
+            l_len.append(len(sent))
+        for sent in l_sent:
+            if len(sent) == max(l_len):
+                return sent
+
+while True:
+    print("\n"+"<<----  MENU  ---->>"+"\n")
+    print("1.) To count the number of dots present --> cd")
+    print("2.) To count the number of 'I' present --> ci")
+    print("3.) To display the lines starting with 'T/t' --> dt")
+    print("4.) To display the first and last line --> fl")
+    print("5.) To display the longest line --> ll")
+    print("6.) To stop the main program --> 0"+"\n")
+    opt = input("Option --> ")
+    opt = opt.lower()
+    if opt in ["cd","ci","dt","fl","ll"]:
+        if opt == "cd":
+            print("\n"+"The number of dots in the poem is,",dots())
+        elif opt == "ci":
+            print("\n"+"The number of times 'I' is repeated is,",count_I())
+        elif opt == "dt":
+            print("\n"+"The following lines are starting with 'T' or 't' :")
+            for elem in lines_tT():
+                print(elem)
+        elif opt == "fl":
+            a,b = lines()
+            print(" ","The following lines are the first and last lines of the poem:","",a,b,sep="\n")
+        elif opt == "ll":
+            print(" ","****  The following line is the longest line :","  "+longest(),sep="\n")
+        else:
+            print("Invalid Option......")
+    elif opt == "0":
+        timer(1)
+        print("\n"+"Thank you for using the string modifier program."+"\n")
+        timer(1)
+        print("Hope to see you soon in string modifier program version 1.2.0"+"\n"+"\n")
+        timer(1.5)
+        print("Program exited with exit code 0"+"\n")
+        break
+    else:
+        print("Invalid Option......")
+
